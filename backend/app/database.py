ac1ba1f57ff16298cbf5628eca475f8cd32da643
Rounak Mitra <rounakmitra9@gmail.com>
2026-03-14 12:57:56 +0530

    Revert "Adding Mockdataset to .gitignore"
    This reverts commit 29fd752ac6adaea68b5dd95fae48af1d931d323d.

--- backend/app/database.py
+++ backend/app/database.py
@@ -14,6 +14,7 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+
 def get_db():
     """Dependency for FastAPI that yields a database session."""
     db = SessionLocal()
